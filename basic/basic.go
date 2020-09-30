c679428f1f37ef07533024713dd0fcb072d55b54
sanju shah <sanjushahgupta@gmail.com>
2020-09-30 15:22:41 +0545

    add index files

--- basic/basic.go
+++ basic/basic.go
@@ -2,6 +2,8 @@ package basic
 
 import "fmt"
 
+var sum, a, b int
+
 func Cal(number1, number2 int) int {
 	var addition, subtraction, multiplication, Mod, Division int
 	addition = number1 + number2
@@ -23,5 +25,12 @@ func Cal(number1, number2 int) int {
 	fmt.Printf("%d modulos %d  = %d", number1, number2, Mod)
 	fmt.Println()
 	fmt.Printf("%d / %d = %d", number1, number2, Division)
+	fmt.Println()
 	return 0
 }
+
+//func add(a, b int) int {
+//sum = a + b
+//fmt.Printf("%d + %d = %d", a, b, sum)
+//return 0
+//}
--- main.go
+++ main.go
@@ -4,13 +4,25 @@ import (
 	"fmt"
 	"net/http"
 
+	"html/template"
+
 	b "github.com/sanjushahgupta/beginner/basic"
 )
 
+func show(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("index.html")
+		t.Execute(w, nil)
+	}
+}
+
 func main() {
+	http.HandleFunc("/show", show)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello world")
 		b.Cal(3, 6)
+		//	b.add(2, 5)
 	})
 
 	http.ListenAndServe(":8080", nil)

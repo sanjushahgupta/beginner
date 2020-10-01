14d732acadf5daa36b696eb45aacdd92612aebf0
sanju shah <sanjushahgupta@gmail.com>
2020-10-01 13:00:51 +0545

    taking value from url and using switch for operator manupulating

--- basic/basic.go
+++ basic/basic.go
@@ -1,28 +1,5 @@
 package basic
 
-// func Opp(number1 int, number2 int) int {
-// 	result := 0
-// 	var operate string
-
-// 	switch operate {
-// 	case "+":
-// 		result = number1 + number2
-// 		break
-// 	case "-":
-// 		result = number1 - number2
-// 	case "*":
-// 		result = number1 * number2
-// 	case "/":
-// 		result = number1 / number2
-// 	case "%":
-// 		result = number1 % number2
-// 	default:
-// 		fmt.Println("Invalid Operation")
-// 	}
-// 	fmt.Printf("%d %s %d = %d", number1, operate, number2, result)
-// 	return 0
-// }
-
 func Add(x int, y int) int {
 
 	return x + y
--- main.go
+++ main.go
@@ -13,50 +13,26 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome to calculator!\n")
 }
 
-func Sum(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	x, _ := strconv.Atoi(ps.ByName("x"))
-
-	y, _ := strconv.Atoi(ps.ByName("y"))
-
-	plus := strconv.Itoa(basic.Add(x, y))
-	w.Write([]byte(plus))
-}
-func Subt(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	x, _ := strconv.Atoi(ps.ByName("x"))
-
-	y, _ := strconv.Atoi(ps.ByName("y"))
-	minus := strconv.Itoa(basic.Sub(x, y))
-	w.Write([]byte(minus))
-}
-
-func Multi(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	x, _ := strconv.Atoi(ps.ByName("x"))
-
-	y, _ := strconv.Atoi(ps.ByName("y"))
-
-	multiply := strconv.Itoa(basic.Mul(x, y))
-
-	w.Write([]byte(multiply))
-}
-func Divd(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	x, _ := strconv.Atoi(ps.ByName("x"))
-
-	y, _ := strconv.Atoi(ps.ByName("y"))
-	divide := strconv.Itoa(basic.Div(x, y))
-
-	w.Write([]byte(divide))
-}
-
 func main() {
 	router := httprouter.New()
+	router.GET("/:x/:operator/:y", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		x, _ := strconv.Atoi(ps.ByName("x"))
 
-	router.GET("/:x/plus/:y", Sum)
+		y, _ := strconv.Atoi(ps.ByName("y"))
+		switch op := ps.ByName("operator"); op {
+		case "plus":
+			w.Write([]byte(fmt.Sprintf("%d + %d = %d", x, y, basic.Add(x, y))))
+		case "minus":
+			w.Write([]byte(fmt.Sprintf("%d - %d = %d", x, y, basic.Sub(x, y))))
+		case "multiply":
+			w.Write([]byte(fmt.Sprintf(" %d * %d = %d", x, y, basic.Mul(x, y))))
+		case "divide":
+			w.Write([]byte(fmt.Sprintf("%d / %d = %d", x, y, basic.Div(x, y))))
+		default:
+			w.Write([]byte(fmt.Sprintf("error: please use %s operators:\n- plus\n- minus\n- multiply\n- divide", op)))
+		}
 
-	router.GET("/:x/minus/:y", Subt)
-	router.GET("/:x/multiply/:y", Multi)
-	router.GET("/:x/divide/:y", Divd)
-
+	})
 	router.GET("/", Index)
-
 	http.ListenAndServe(":8080", router)
 }
